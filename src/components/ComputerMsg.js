219e4f688fea10be0e8d126aa8c295c2f6f10e05
Dave Tyson <davetysonbass@gmail.com>
2023-03-29 08:21:36 -0400

    another small typo fix to response array

--- src/components/ComputerMsg.js
+++ src/components/ComputerMsg.js
@@ -43,7 +43,7 @@ const ComputerMsg = (props) => {
             "You can do this!",
             "Cardbot: one; you: zero. Or whatever the actual score is",
             "Does this come with a cash prize?",
-            "runVictoryDance(IWin)",
+            "runVictoryDance(iWin)",
             "That's not a knife, THIS is a knife.",
             "Winner winner, chicken dinner!",
             "Victory is mine!",
