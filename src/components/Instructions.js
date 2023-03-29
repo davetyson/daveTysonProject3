9dfaffd7168bd5dee67f9080fbbe4e16e0534c38
Dave Tyson <davetysonbass@gmail.com>
2023-03-28 20:34:22 -0400

    small typo fix in new instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -4,7 +4,7 @@ const Instructions = () => {
         // Instructions block
         <section className="instructions">
             <h3>Instructions</h3>
-            <p>Click to draw a card and see if you can beat the computer! Highest card wins each round. Use the Game Mode buttons to change the gameplay! Aces can be changed to high or low. "Infinite Play" allows you to play forever, and in "First to 21" mode, the first person to reach 21 points wins the game (please note, selecting "First to 21" will reset the scores so you can each start the game at 0). Page defaults to ace high and "Infinite Play".</p>
+            <p>Click to draw a card and see if you can beat the computer! Highest card wins each round. Use the Game Mode buttons to change the gameplay! Aces can be changed to high or low. "Infinite Play" allows you to play forever, and in "First to 21" mode, the first person to reach 21 points wins the game (please note, selecting "First to 21" will reset the scores so you can each start the game at 0). Page defaults to ace high and "Infinite Play."</p>
         </section>
     )
 }
