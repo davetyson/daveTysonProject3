288712a8225728b0e7b4fb8b6ce7eecaf1c5b935
Dave Tyson <davetysonbass@gmail.com>
2023-03-28 21:55:34 -0400

    moved deck of cards api dev credit to the instructions area

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import CardGame from './components/CardGame';
 import Instructions from './components/Instructions';
 import Footer from './components/Footer';
 
-// Thanks to Chase Roberts for the Deck of Cards API :) https://deckofcardsapi.com/
-
 function App() {
   return (
     <div className="App wrapper">
